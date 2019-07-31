199b3b84607e09b70327a48ba87c9a0cac62dc15
yangxinpeng <972672845@qq.com>
2019-07-31 23:12:42 +0800

    首页完成

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,13 +1,119 @@
 <template>
-    <div class="box"></div>
+  <el-container class="listContainer">
+    <el-aside :width="isout?'200px':'64px'" class="list">
+      <el-menu
+        default-active="1"
+        background-color="#002033"
+        text-color="#fff"
+        active-text-color="#ffd04b"
+        :collapse='!isout'
+        :collapse-transition='false'
+      >
+        <el-menu-item index="1">
+          <i class="el-icon-s-home"></i>
+          <span slot="title">首页</span>
+        </el-menu-item>
+        <el-menu-item index="2">
+          <i class="el-icon-menu"></i>
+          <span slot="title">内容管理</span>
+        </el-menu-item>
+        <el-menu-item index="3">
+          <i class="el-icon-document"></i>
+          <span slot="title">素材管理</span>
+        </el-menu-item>
+        <el-menu-item index="4">
+          <i class="el-icon-setting"></i>
+          <span slot="title">发布文章</span>
+        </el-menu-item>
+        <el-menu-item index="5">
+          <i class="el-icon-setting"></i>
+          <span slot="title">评论管理</span>
+        </el-menu-item>
+        <el-menu-item index="6">
+          <i class="el-icon-setting"></i>
+          <span slot="title">粉丝管理</span>
+        </el-menu-item>
+        <el-menu-item index="7">
+          <i class="el-icon-setting"></i>
+          <span slot="title">个人设置</span>
+        </el-menu-item>
+      </el-menu>
+    </el-aside>
+    <el-container>
+      <el-header>
+        <span class="el-icon-s-fold" @click="tab()"></span>
+        <span>江苏传智播客教育科技有限公司</span>
+        <div class="right">
+          <img src="../../assets/images/avatar.jpg" />
+          <el-dropdown>
+            <span class="el-dropdown-link">
+              下拉菜单
+              <i class="el-icon-arrow-down el-icon--right"></i>
+            </span>
+            <el-dropdown-menu slot="dropdown">
+              <el-dropdown-item>黄金糕</el-dropdown-item>
+            </el-dropdown-menu>
+          </el-dropdown>
+        </div>
+      </el-header>
+      <el-main class="img">
+        <img src="../../assets/images/welcome.jpg" alt />
+      </el-main>
+    </el-container>
+  </el-container>
 </template>
 
 <script>
 export default {
-
-}
+  data() {
+    return {
+      isout: true
+    };
+  },
+  methods: {
+    tab() {
+      this.isout = !this.isout;
+    }
+  }
+};
 </script>
 
-<style>
-
+<style scoped lang="less">
+.listContainer {
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  left: 0;
+  top: 0;
+  .list {
+    background: #002033;
+    width: 200px;
+  }
+  .el-header {
+    line-height: 60px;
+    .right {
+      float: right;
+      line-height: 60px;
+      img {
+        width: 30px;
+        height: 30px;
+        vertical-align: middle;
+        margin-right: 10px;
+      }
+    }
+    .el-icon-s-fold {
+      font-size: 26px;
+      vertical-align: middle;
+    }
+  }
+  .img {
+    position: relative;
+    img {
+      position: absolute;
+      top: 50%;
+      left: 50%;
+      transform: translate(-50%, -50%);
+    }
+  }
+}
 </style>
