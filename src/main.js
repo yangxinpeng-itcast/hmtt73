9b7a79744ab5cb0c5b91751503d94c74bf5b1d90
yangxinpeng <972672845@qq.com>
2019-07-31 18:52:40 +0800

    登录功能实现

--- src/main.js
+++ src/main.js
@@ -6,8 +6,12 @@ import router from '@/router'
 import ElementUI from 'element-ui';
 // 引入样式
 import 'element-ui/lib/theme-chalk/index.css';
+import axios from 'axios'
+
+
 // 在全局范围注册组件（注册element-ui组件库）
 Vue.use(ElementUI)
+Vue.prototype.$axios = axios 
 
 Vue.config.productionTip = false
 
--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -4,22 +4,23 @@
       <div class="img">
         <img src="../../assets/images/logo_index.png" alt />
       </div>
-      <el-form :model="formData">
-        <el-form-item>
+      <el-form :model="formData" :rules="rules" ref="formData">
+        <el-form-item prop="mobile">
           <el-input v-model="formData.mobile"></el-input>
         </el-form-item>
-        <el-form-item style="width:80%;display:inline-block">
-          <el-input  v-model="formData.code"></el-input>
+        <el-form-item style="width:80%;display:inline-block" prop="code">
+          <el-input v-model="formData.code"></el-input>
         </el-form-item>
         <el-form-item style="display:inline-block;float:right">
           <el-button>验证码</el-button>
         </el-form-item>
         <el-form-item>
-             <el-checkbox v-model="checked"></el-checkbox>
-             我已阅读并同意<el-link type="primary" :underline="false">用户协议</el-link>和<el-link type="primary" :underline="false">隐私条款</el-link>
+          <el-checkbox v-model="checked"></el-checkbox>我已阅读并同意
+          <el-link type="primary" :underline="false">用户协议</el-link>和
+          <el-link type="primary" :underline="false">隐私条款</el-link>
         </el-form-item>
         <el-form-item style="width:100%">
-            <el-button  style="width:100%" type="primary" >登录</el-button>
+          <el-button style="width:100%" type="primary" @click="submitForm()">登录</el-button>
         </el-form-item>
       </el-form>
     </el-card>
@@ -29,13 +30,46 @@
 <script>
 export default {
   data() {
+    //自定义校验手机号
+    const checkmobile = (rule, value, callback) => {
+      if (!value) {
+        return callback(new Error("手机号不为空"));
+      }
+      // 校验逻辑
+      else if (/^1[3-9]\d{9}$/.test(value)) {
+        callback();
+      } else {
+        callback(new Error("手机号格式不对"));
+      }
+    };
     return {
       formData: {
-        mobile: 13911111111,
-        code: 246810
+        mobile: '13911111111',
+        code: '246810'
       },
-      checked:true
+      checked: true,
+      rules: {
+        mobile: [{ validator: checkmobile, trigger: "blur" }],
+        code: [{ required: true, message: "请输入验证码", trigger: "blur" }]
+      }
     };
+  },
+  methods: {
+    submitForm() {
+      this.$refs.formData.validate(async valid => {
+        if (valid) {
+          const { data:{data} } =await this.$axios({
+            method: "post",
+            url: "http://ttapi.research.itcast.cn/mp/v1_0/authorizations",
+            data: this.formData
+          });
+          // console.log(data)
+        } else {
+          console.log("登陆失败");
+          return false;
+        }
+      });
+    }
   }
 };
 </script>
